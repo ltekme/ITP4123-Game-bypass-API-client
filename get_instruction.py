fc4346dd53201ba7dcf32d321db91c8979e36ab2
Karl <karl@ltek.me>
2024-11-21 03:45:04 +0000

    update get quest

--- get_instruction.py
+++ get_instruction.py
@@ -1,8 +1,30 @@
+import time
+import requests
+import json
+import os
+from _modules import dump_to_json, get_data_from_json, call_api
 from _modules import get_data_from_json
 
-quests = get_data_from_json('./data/quests.json')
+
+def _get_unfinished_quests():
+    while True:
+        api_response = call_api(path="game?mode")
+        if api_response.status_code != 200:
+            raise Exception(f"Error: {api_response.status_code}")
+        data = api_response.content.decode('utf-8')
+        if "[" not in data:
+            continue
+        return json.loads(str(data))
+
 
 if __name__ == "__main__":
+    quests_file = './data/quests.json'
+
+    if not os.path.exists(quests_file):
+        dump_to_json(quests_file, _get_unfinished_quests())
+
+    quests = get_data_from_json(quests_file)
+
     for i in quests:
         print('*'*50)
         test_name = i['tests'][0]
